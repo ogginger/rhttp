bab56a476e76ed44a44fd128ff5ba7136c5e2e1f
Josh <og.ginger@gmail.com>
2018-04-23 23:36:32 +0000

    Edited main.js: Abstracted requirejs configuration into another file, and injected via node require.

--- main.js
+++ main.js
@@ -2,60 +2,9 @@
 /* This file depends on nodejs and the nodejs module requirejs. */
 
 var requirejs = require("requirejs");
+var config = require("./require_config");
 
-requirejs.config({
-        nodeRequire: require,
-        baseUrl: "/*Project*/",
-        paths: {
-		"rsvp": "../lib/rsvp.min",
-		"backbone": "../lib/backbone.min",
-		"underscore": "../lib/underscore.min"
-	},
-        bundles: {
-		"../lib/snip.min": [ "Snip" ],
-		"../lib/file.min": [ "file" ],
-                "../lib/testsuite.min": [ "TestSuite" ],
-                "../lib/promise.min": [ "promise" ]
-	},
-	suppress: {
-		nodeShim: true
-	},
-	shim: {
-		"underscore": {
-			deps: [
-				"jquery"
-			],
-			exports: "_"
-		},
-		"backbone": {
-			deps: [
-				"underscore"
-			],
-			exports: "Backbone"
-		},
-		"file": {
-			deps: [
-				"fs",
-				"rsvp"
-			],
-			exports: "file"
-		},
-		"Snip": {
-			deps: [
-				"file",
-				"backbone"
-			],
-			exports: "Snip"
-		},
-		"promise": {
-                        deps: [
-                                "rsvp"
-                        ],
-                        exports: "promise"
-                }
-
-	}
-});
+requirejs.config(config);
 
 requirejs([
 	"file",
